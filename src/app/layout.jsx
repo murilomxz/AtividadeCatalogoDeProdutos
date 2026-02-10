8a51c335f4d121ecec20a0c9bcfad7bfa7547790
MURILO MACHADO BRANDALEZI <25170098@educ123.sp.senai.br>
2026-02-10 11:14:25 -0300

    some changes

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,7 +2,7 @@
 
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Footer from "@/components/blocks/navbar/footer/page";
+import Footer from "@/components/blocks/footer/page";
 import Navbar from "@/components/blocks/navbar/page";
 import "bootstrap/dist/css/bootstrap.min.css";
 
